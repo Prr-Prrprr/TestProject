871605aeea9359c63d6bb610c2f83d7ddda88bdb
MarinaM <meowm5476@gmail.com>
2022-11-14 00:43:01 +0000

    add profiles for users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :categories, only: [:index, :show]
+  resources :users, only: [:show]
   
   get 'doctors', to: 'doctors#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
